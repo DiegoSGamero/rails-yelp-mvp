32c2e0275fcae368f1623dd26dd5e16acda1cc5d
diego joao <diegosgjoao@gmail.com>
2023-08-23 17:10:03 -0300

    method create done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  root to: "restaurants#index"
+  resources :restaurants
 end
